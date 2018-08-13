c8e78d3aad43802999f2ae95f37eee4d70821576
34735 <zhanggong@rainbowcn.com>
2018-08-13 20:41:29 +0800

    模板方法模式

--- src/main/java/tech/zg/patterns/behavior/behavior2_template_method_patterns/MainClient.java
+++ src/main/java/tech/zg/patterns/behavior/behavior2_template_method_patterns/MainClient.java
@@ -1,7 +1,7 @@
 package tech.zg.patterns.behavior.behavior2_template_method_patterns;
 
 /**
- * 测试主类
+ * 模板方法模式测试主类
  * <p>
  *
  * @author: 张弓
@@ -12,5 +12,22 @@ public class MainClient {
 
     public static void main(String[] args) {
 
+        AbstractTemplate template1 = new ConcreteTemplate1();
+        AbstractTemplate template2 = new ConcreteTemplate2();
+
+        // 这里直接执行模板方法，模板方法会根据不同的具体类，调用具体的方法。
+        template1.templateMethod();
+        System.out.println();
+        template2.templateMethod();
+
+        /**
+         * doSomething1
+         I Am ConcreteTemplate1 DoSomething2
+         doSomething3
+
+         doSomething1
+         I Am ConcreteTemplate2 DoSomething2
+         doSomething3
+         */
     }
 }
