0c4a82d37b4d3753070e4f1667c085055b87dbd1
34735 <zhanggong@rainbowcn.com>
2018-08-13 20:52:39 +0800

    外观模式

---
+++ src/main/java/tech/zg/patterns/structure/structure7_facade_patterns/MainClient.java
@@ -0,0 +1,12 @@
+package tech.zg.patterns.structure.structure7_facade_patterns;
+
+/**
+ * 外观模式测试主类
+ * <p>
+ *
+ * @author: 张弓
+ * @date: 2018/8/13
+ * @version: 1.0.0
+ */
+public class MainClient {
+}
