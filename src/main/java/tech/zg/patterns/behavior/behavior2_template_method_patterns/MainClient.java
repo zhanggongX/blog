98cd7c97ed2729be0fa471e819be13409bc784e4
34735 <zhanggong@rainbowcn.com>
2018-08-13 20:25:18 +0800

    模板方法模式

---
+++ src/main/java/tech/zg/patterns/behavior/behavior2_template_method_patterns/MainClient.java
@@ -0,0 +1,16 @@
+package tech.zg.patterns.behavior.behavior2_template_method_patterns;
+
+/**
+ * 测试主类
+ * <p>
+ *
+ * @author: 张弓
+ * @date: 2018/8/13
+ * @version: 1.0.0
+ */
+public class MainClient {
+
+    public static void main(String[] args) {
+
+    }
+}
