20e90325de78e59d5fa114fe34d62a338b90b8da
34735 <zhanggong@rainbowcn.com>
2018-08-16 08:40:18 +0800

    建造者模式

---
+++ src/main/java/tech/zg/patterns/create/create5_builder_patterns/MainClient.java
@@ -0,0 +1,13 @@
+package tech.zg.patterns.create.create5_builder_patterns;
+
+/**
+ * 建造者模式主类
+ * <p>
+ *
+ * @author: 张弓
+ * @date: 2018/8/15
+ * @version: 1.0.0
+ */
+public class MainClient {
+
+}
