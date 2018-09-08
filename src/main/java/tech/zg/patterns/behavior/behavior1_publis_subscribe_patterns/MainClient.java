55a3438a2b23058707e4cca8460e741de2e8f4ae
34735 <zhanggong@rainbowcn.com>
2018-09-08 16:26:36 +0800

    观察者模式

--- src/main/java/tech/zg/patterns/behavior/behavior1_publis_subscribe_patterns/MainClient.java
+++ src/main/java/tech/zg/patterns/behavior/behavior1_publis_subscribe_patterns/MainClient.java
@@ -2,6 +2,9 @@ package tech.zg.patterns.behavior.behavior1_publis_subscribe_patterns;
 
 import tech.zg.patterns.behavior.behavior1_publis_subscribe_patterns.v1.ConcreteObServer;
 import tech.zg.patterns.behavior.behavior1_publis_subscribe_patterns.v1.ConcreteSubject;
+import tech.zg.patterns.behavior.behavior1_publis_subscribe_patterns.v2.EventHandler;
+import tech.zg.patterns.behavior.behavior1_publis_subscribe_patterns.v2.ListenerOne;
+import tech.zg.patterns.behavior.behavior1_publis_subscribe_patterns.v2.ListenerTwo;
 
 /**
  * 观察者模式主类
@@ -25,6 +28,15 @@ public class MainClient {
         concreteSubject.notifyMsg();
 
         // 观察者模式进阶，委托模式
+        EventHandler eventHandler = new EventHandler();
+        eventHandler.addEvent(new ListenerOne(), "doSomeThingOne", "吃饭");
+        eventHandler.addEvent(new ListenerTwo(), "doSomeThingTwo", "睡觉", 1);
+
+        try {
+            eventHandler.notifyUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
---
+++ src/main/java/tech/zg/patterns/behavior/behavior1_publis_subscribe_patterns/v2/ConcreteNotifier.java
@@ -0,0 +1,29 @@
+package tech.zg.patterns.behavior.behavior1_publis_subscribe_patterns.v2;
+
+/**
+ * 具体的通知者
+ * <p>
+ *
+ * @author: 张弓
+ * @date: 2018/9/8
+ * @version: 1.0.0
+ */
+public class ConcreteNotifier extends Notifier {
+
+    /**
+     * 通知，具体的通知，由具体的通知者实现
+     * <p>
+     *
+     * @author: 张弓
+     * @date: 2018/9/8
+     * @version: 1.0.0
+     */
+    @Override
+    public void notifyUpdate() {
+        try {
+            this.eventHandler.notifyUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
